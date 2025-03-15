14cd842eb9f816bbf44807e352e5877a83e18bc0
mohmedpatel7 <153270796+mohmedpatel7@users.noreply.github.com>
2025-03-15 15:11:17 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // ✅ Import BrowserRouter
 import App from "./App.jsx";
 import { ToastProvider } from "./components/Genral/ToastContext.jsx";
 import { Provider } from "react-redux";
@@ -9,7 +10,9 @@ createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <ToastProvider>
       <StrictMode>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </StrictMode>
     </ToastProvider>
   </Provider>
